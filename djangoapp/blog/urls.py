c4b7c81c115f824b60a6f45324e2b3ae4f84ea4d
danielcarmofigueiredo <danielcarmofigueiredo@gmail.com>
2024-12-09 16:31:46 -0300

    Convertendo a FVB page para uma CBV PageDetailView

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -16,17 +16,18 @@ Including another URLconf
 """
 from django.urls import path
 # from blog.views import index, post, page, created_by, category, tag, search
-from blog.views import page, search, post
+from blog.views import page, post
 
 from blog.views import (PostListView, CreatedByListView,
-                        CategoryListView, TagListView, SearchListView)
+                        CategoryListView, TagListView, SearchListView,
+                        PageDetailView)
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', PostListView.as_view(), name='index'),
     path('post/<slug:slug>/', post, name='post'),
-    path('page/<slug:slug>/', page, name='page'),
+    path('page/<slug:slug>/', PageDetailView.as_view(), name='page'),
     path('created_by/<int:author_pk>/',
          CreatedByListView.as_view(), name='created_by'),
     path('category/<slug:slug>/', CategoryListView.as_view(), name='category'),
--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -7,7 +7,7 @@ from django.db.models import Q
 from django.contrib.auth.models import User
 from django.http import Http404, HttpRequest, HttpResponse
 
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 PER_PAGE = 9
 
@@ -143,6 +143,25 @@ def tag(request, slug):
     )
 
 
+class PageDetailView(DetailView):
+    model = Page
+    template_name = 'blog/pages/page.html'
+    slug_field = 'slug'
+    context_object_name = 'page'
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        page = self.get_object()
+        page_title = f'{page.title} - Página - '  # type: ignore
+        context.update({
+            'page_title': page_title,
+        })
+        return context
+
+    def get_queryset(self) -> QuerySet[Any]:
+        return super().get_queryset().filter(is_published=True)
+
+
 def page(request, slug):
     page_obj = Page\
         .objects\
@@ -211,29 +230,3 @@ class SearchListView(PostListView):
             return redirect('blog:index')
 
         return super().get(request, *args, **kwargs)
-
-
-def search(request):
-    search_value = request.GET.get('search', '').strip()
-    posts = (Post
-             .objects
-             .get_published()  # type: ignore
-             .filter(
-                 # Título contém o search_value OU
-                 # Excerto contém o search_value OU
-                 # Conteúdo contém o search_value
-                 Q(title__icontains=search_value) |
-                 Q(excerpt__icontains=search_value) |
-                 Q(content__icontains=search_value)
-             )[:PER_PAGE]
-             )
-    page_title = f'{search_value[:30]} - Search - '
-    return render(
-        request,
-        'blog/pages/index.html',
-        {
-            'page_obj': posts,
-            'search_value': search_value,
-            'page_title': page_title,
-        }
-    )
